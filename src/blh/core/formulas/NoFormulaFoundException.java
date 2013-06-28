c24559f8e5f993c722281047803bee2267025085
Erik Larkö <erik.larko@purplescout.se>
2013-06-28 17:07:31 +0200

    Added rudimentary Dependency thingie by singleton class

---
+++ src/blh/core/formulas/NoFormulaFoundException.java
@@ -0,0 +1,18 @@
+package blh.core.formulas;
+
+/**
+ * Created by Erik Larkö at 6/28/13 1:31 PM
+ */
+public class NoFormulaFoundException extends Exception {
+
+    public NoFormulaFoundException() {
+    }
+
+    public NoFormulaFoundException(String message) {
+        super(message);
+    }
+
+    public NoFormulaFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ src/blh/core/formulas/NoMatchingFormulaFoundException.java
@@ -0,0 +1,18 @@
+package blh.core.formulas;
+
+/**
+ * Created by Erik Larkö at 6/28/13 1:30 PM
+ */
+public class NoMatchingFormulaFoundException extends Exception {
+
+    public NoMatchingFormulaFoundException() {
+    }
+
+    public NoMatchingFormulaFoundException(String message) {
+        super(message);
+    }
+
+    public NoMatchingFormulaFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
