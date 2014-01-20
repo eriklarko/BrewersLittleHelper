32aee8dedaf46bccb188bc9907bb35d03e99bbc8
Erik Larkö <erik.larko@purplescout.se>
2014-01-20 17:02:20 +0100

    Made the additions in IngredientsList observable

--- recipe/src/main/java/org/blh/recipe/IngredientsList.java
+++ recipe/src/main/java/org/blh/recipe/IngredientsList.java
@@ -2,6 +2,7 @@ package org.blh.recipe;
 
 import java.util.Collection;
 import java.util.LinkedList;
+
 import org.blh.core.recipe.GristPart;
 import org.blh.core.recipe.HopAddition;
 import org.blh.core.recipe.YeastAddition;
@@ -11,66 +12,75 @@ import org.blh.core.units.weight.Grams;
 import org.blh.core.units.weight.Kilograms;
 import org.blh.core.units.weight.Lbs;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 /**
- * TODO: It seems that a lot of things here should be observable...
- * All the calculating methods should be observable imo...
+ * TODO: It seems that a lot of things here should be observable... All the
+ * calculating methods should be observable imo...
  *
  * @author thinner
  */
 public class IngredientsList {
 
-    private final Collection<GristPart> fermentables;
-    private final Collection<HopAddition> hopAdditions;
-    private final Collection<YeastAddition<?>> yeastAdditions;
+	private final ObservableList<GristPart> fermentables;
+	private final ObservableList<HopAddition> hopAdditions;
+	private final ObservableList<YeastAddition<?>> yeastAdditions;
 
 	public IngredientsList(Collection<GristPart> fermentables, Collection<HopAddition> hopAdditions, Collection<YeastAddition<?>> yeastAdditions) {
+		this(FXCollections.observableArrayList(fermentables),
+			FXCollections.observableArrayList(hopAdditions),
+			FXCollections.observableArrayList(yeastAdditions));
+	}
+
+	public IngredientsList(ObservableList<GristPart> fermentables, ObservableList<HopAddition> hopAdditions, ObservableList<YeastAddition<?>> yeastAdditions) {
 		this.fermentables = fermentables;
 		this.hopAdditions = hopAdditions;
 		this.yeastAdditions = yeastAdditions;
 	}
 
-    public Collection<GristPart> getFermentables() {
-        return fermentables;
-    }
+	public Collection<GristPart> getFermentables() {
+		return fermentables;
+	}
 
-    public Collection<HopAddition> getHopAdditions() {
-        return new LinkedList<>(hopAdditions);
-    }
+	public Collection<HopAddition> getHopAdditions() {
+		return new LinkedList<>(hopAdditions);
+	}
 
 	public Collection<YeastAddition<?>> getYeastAdditions() {
 		return new LinkedList<>(yeastAdditions);
 	}
 
-    public Kilograms getTotalGrainWeight() {
-        double totalWeight = 0;
-        for (GristPart fermentable : fermentables) {
-            totalWeight += fermentable.getMalt().getColor().value();
-        }
-        return new Kilograms(totalWeight);
-    }
+	public Kilograms getTotalGrainWeight() {
+		double totalWeight = 0;
+		for (GristPart fermentable : fermentables) {
+			totalWeight += fermentable.getMalt().getColor().value();
+		}
+		return new Kilograms(totalWeight);
+	}
 
-    public GravityPoints getTotalGravityPoints() {
-        double specificGravityPoints = 0;
-        for (GristPart fermentable : fermentables) {
-            specificGravityPoints += fermentable.getMalt().getExtractPotential().value();
-        }
+	public GravityPoints getTotalGravityPoints() {
+		double specificGravityPoints = 0;
+		for (GristPart fermentable : fermentables) {
+			specificGravityPoints += fermentable.getMalt().getExtractPotential().value();
+		}
 
-        return new GravityPoints(specificGravityPoints);
-    }
+		return new GravityPoints(specificGravityPoints);
+	}
 
-    public ColorPotential getTotalColorPotential() {
-        ColorPotential potential = new ColorPotential();
-        for (GristPart fermentable : fermentables) {
-            potential.add(fermentable.getMalt().getColor(), new Lbs(fermentable.getAmount()));
-        }
-        return potential;
-    }
+	public ColorPotential getTotalColorPotential() {
+		ColorPotential potential = new ColorPotential();
+		for (GristPart fermentable : fermentables) {
+			potential.add(fermentable.getMalt().getColor(), new Lbs(fermentable.getAmount()));
+		}
+		return potential;
+	}
 
-    public Grams getTotalHopWeight() {
-        double totalGrams = 0;
-        for (HopAddition addition : hopAdditions) {
-            totalGrams += addition.getAmount().value();
-        }
-        return new Grams(totalGrams);
-    }
+	public Grams getTotalHopWeight() {
+		double totalGrams = 0;
+		for (HopAddition addition : hopAdditions) {
+			totalGrams += addition.getAmount().value();
+		}
+		return new Grams(totalGrams);
+	}
 }
