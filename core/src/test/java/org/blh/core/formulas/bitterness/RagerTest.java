5af5d49a10022194bf7813e5ef0f9c7d67f4da3e
Erik Larkö <erik.larko@purplescout.se>
2014-01-20 10:35:47 +0100

    Fixed Rager test

--- core/src/test/java/org/blh/core/formulas/bitterness/RagerTest.java
+++ core/src/test/java/org/blh/core/formulas/bitterness/RagerTest.java
@@ -1,5 +1,8 @@
 package org.blh.core.formulas.bitterness;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.blh.core.ingredients.Hop;
 import org.blh.core.recipe.HopAddition;
 import org.blh.core.units.Percentage;
@@ -7,10 +10,8 @@ import org.blh.core.units.bitterness.IBU;
 import org.blh.core.units.gravity.SpecificGravity;
 import org.blh.core.units.time.Minutes;
 import org.blh.core.units.volume.Liters;
-import org.blh.core.units.weight.Grams;
-import java.util.Arrays;
-import java.util.List;
 import org.blh.core.units.volume.USGallons;
+import org.blh.core.units.weight.Grams;
 import org.blh.core.units.weight.Oz;
 import org.junit.Assert;
 import org.junit.Test;
@@ -20,21 +21,23 @@ import org.junit.Test;
  * @author thinner
  */
 public class RagerTest {
-    
+
     @Test
     public void testCalc() {
         HopAddition addition1 = new HopAddition(new Hop(null, new Percentage(5)), new Minutes(60), new Grams(50));
         HopAddition addition2 = new HopAddition(new Hop(null, new Percentage(7)), new Minutes(25), new Grams(85));
         List<HopAddition> hopAdditions = Arrays.asList(addition1, addition2);
-        
+
         Liters boilVolume = new Liters(18);
         SpecificGravity boilGravity = new SpecificGravity(1.050);
-        
+
         Rager f = new Rager();
         IBU actual = f.calc(hopAdditions, boilVolume, boilGravity);
         IBU expected = new IBU(87.423);
-        
-        Assert.assertEquals(expected.value(), actual.value());
+
+		actual.setDelta(3);
+		expected.setDelta(3);
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
@@ -43,11 +46,13 @@ public class RagerTest {
         HopAddition addition = new HopAddition(hop, new Minutes(45), new Oz(1.5).toGrams());
         Liters boilVolume = new USGallons(5).toLiters();
         SpecificGravity boilGravity = new SpecificGravity(1.045);
-        
+
         Rager f = new Rager();
         IBU actual = f.getIBUsFromAddition(addition, boilVolume, boilGravity);
         IBU expected = new IBU(48.355);
-        
-        Assert.assertEquals(expected.value(), actual.value());
+
+		actual.setDelta(3);
+		expected.setDelta(3);
+        Assert.assertEquals(expected, actual);
     }
 }
