4c676e0c062b7d6b5075bf95c95728758d5600a1
Erik Larkö <erik.larko@purplescout.se>
2014-05-22 23:27:55 +0200

    Based FormulaFactory on a multimap

--- anders/src/main/java/se/angstroms/blh/anders/uncategorized/value/findingformulas/FormulaFactory.java
+++ anders/src/main/java/se/angstroms/blh/anders/uncategorized/value/findingformulas/FormulaFactory.java
@@ -1,7 +1,8 @@
 package se.angstroms.blh.anders.uncategorized.value.findingformulas;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+import java.util.Collection;
 import org.blh.core.unit.Unit;
 import se.angstroms.blh.anders.formulas.ObservableFormula;
 import se.angstroms.blh.anders.uncategorized.value.ValueId;
@@ -12,21 +13,25 @@ import se.angstroms.blh.anders.uncategorized.value.ValueId;
  */
 public class FormulaFactory {
 
-	private final Map<ValueId, ObservableFormula<?>> map;
+	private final Multimap<ValueId, ObservableFormula<?>> map;
 
 	public FormulaFactory() {
-		map = new HashMap<>();
+		map = HashMultimap.create();
 	}
 
 	public void register(ValueId type, ObservableFormula<?> formula) {
 		map.put(type, formula);
 	}
 
+    public Collection<ObservableFormula<?>> getFormulasFor(ValueId type) {
+        return map.get(type);
+    }
+
 	public <T extends Unit<?>> ObservableFormula<T> getDefaultFormula(ValueId type) throws NoDefaultFormulaException {
-		ObservableFormula<T> defaultFormula = (ObservableFormula<T>) map.get(type);
-		if (defaultFormula == null) {
+        Collection<ObservableFormula<?>> formulas = getFormulasFor(type);
+		if (formulas.isEmpty()) {
 			throw new NoDefaultFormulaException("No default formula for " + type.name() + " found.");
 		}
-		return defaultFormula;
+		return (ObservableFormula<T>) formulas.iterator().next();
 	}
 }
